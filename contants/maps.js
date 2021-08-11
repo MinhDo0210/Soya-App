c1e5f052961e60d835ab01480cfbbf2a9b9742a5
MinhDo0210 <minhdohungkhanh@gmail.com>
2021-08-11 10:54:50 +0700

    first commit

---
+++ contants/maps.js
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+const GOOGLE_API_KEY = 'AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ';
+
+export default GOOGLE_API_KEY;
---
+++ screens/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import Home from './Home';
+import Restaurant from './Restaurant';
+import OrderDelivery from './OrderDelivery';
+
+export {
+    Home,
+    Restaurant,
+    OrderDelivery,
+};
